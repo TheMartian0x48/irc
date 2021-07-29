4b270ff02f4b4f950795923c6550826f340e4221
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2021-07-29 23:10:52 +0530

    Updated : introducted new global data active_users

--- src/global.cpp
+++ src/global.cpp
@@ -1,4 +1,7 @@
 #include "global.hpp"
 
 Server irc_server = Server(std::string("v1.0"), std::string("what password?"),
-                         std::string("what info?"));
+                           std::string("what info?"));
+
+ThreadSafeMap<std::string, std::shared_ptr<Client>> active_users =
+    ThreadSafeMap<std::string, std::shared_ptr<Client>>();
--- src/global.hpp
+++ src/global.hpp
@@ -1,8 +1,14 @@
 #ifndef IRC_SRC_GLOBAL_HPP
 #define IRC_SRC_GLOBAL_HPP
 
+#include <memory>
+#include <string>
+
+#include "client.hpp"
 #include "server.hpp"
+#include "thread_safe_map.hpp"
 
 extern Server irc_server;
+extern ThreadSafeMap<std::string, std::shared_ptr<Client>> active_users;
 
 #endif  // IRC_SRC_GLOBAL_HPP_
