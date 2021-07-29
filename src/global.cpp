97b2918c4b516215e96ef2fb4c62038e11fb3d91
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2021-07-29 22:22:16 +0530

    Added: global data for whole project

---
+++ src/global.cpp
@@ -0,0 +1,4 @@
+#include "global.hpp"
+
+Server irc_server = Server(std::string("v1.0"), std::string("what password?"),
+                         std::string("what info?"));
---
+++ src/global.hpp
@@ -0,0 +1,8 @@
+#ifndef IRC_SRC_GLOBAL_HPP
+#define IRC_SRC_GLOBAL_HPP
+
+#include "server.hpp"
+
+extern Server irc_server;
+
+#endif  // IRC_SRC_GLOBAL_HPP_
